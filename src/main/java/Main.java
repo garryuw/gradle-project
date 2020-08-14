6aaf1219299b29eae888128c76a484fdcf139e3b
Garry <garryuw@gmail.com>
2020-08-14 11:51:34 -0700

    gradle_TC-build

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import org.apache.commons.lang3.StringUtils;
-
-class Main {
-	public static void main(String[] args){
-		System.out.println("Hello world");
-        System.out.println(StringUtils.isBlank(" "));
-	}
-}
